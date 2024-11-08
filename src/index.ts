fd1f6f40273d496f17336d8f09b4b13e1c9e8b9a
42 <42@koishi.chat>
2024-11-08 14:43:38 +0800

    文本优化

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export interface Config {
 
 export const Config: Schema<Config> = Schema.object({
   endpoint: Schema.string().default('http://127.0.0.1:7860').description('sd-webui 的地址'),
-  upscaler: Schema.string().default('SwinIR_4x').description('默认采样器'),
+  upscaler: Schema.string().default('SwinIR_4x').description('默认放大模型'),
   resize: Schema.number().default(2).min(0).max(4).description('默认放大倍数'),
 })
 
@@ -33,23 +33,23 @@ export function apply(ctx: Context, config: Config) {
   ctx.command(name, 'sd-extra')
     .alias('放大')
     .option('resize', '-r <resize:number> 放大倍数', { type: resize })
-    .option('upscaler', '-u <upscaler:text> 手动选择采样器')
-    .option('switch', '-x <switch:boolean> 切换默认采样器')
-    .option('show', '-s <show:boolean> 显示可用采样器')
+    .option('upscaler', '-u <upscaler:text> 单次选择放大模型')
+    .option('switch', '-x <switch:boolean> 切换默认放大模型')
+    .option('show', '-s <show:boolean> 显示可用放大模型')
     .action(async ({ session, options }, input) => {
       options.resize ??= config.resize
       options.upscaler ??= config.upscaler
       if (options.switch) {
         const upscalers = await getUpscalers()
         const upscalerList = upscalers.map((upscaler, index) => `${index + 1}. ${upscaler}`).join('\n')
-        session.send(`当前采样器为：${options.upscaler}\n可用的采样器有：\n${upscalerList}\n请输入要切换的采样器序号：`)
+        session.send(`当前放大模型为：${options.upscaler}\n可用的放大模型有：\n${upscalerList}\n请输入要切换的放大模型序号：`)
         const index = parseInt(await session.prompt())
         if (isNaN(index) || index < 1 || index > upscalers.length) return '输入错误'
         config.upscaler = upscalers[index - 1]
         ctx.scope.update(config, false)
-        return `已将默认采样器切换为 ${upscalers[index - 1]}`
+        return `已将默认放大模型切换为 ${upscalers[index - 1]}`
       }
-      if (options.show) return `当前采样器为：${options.upscaler}\n可用的采样器有：\n${(await getUpscalers()).join('\n')}`
+      if (options.show) return `当前放大模型为：${options.upscaler}\n可用的放大模型有：\n${(await getUpscalers()).join('\n')}`
       const imgUrl = h.select(input, 'img')[0]?.attrs.src
       if (!imgUrl) return session.execute(`help ${name}`)
       const img = await ctx.http.get(imgUrl, { responseType: 'arraybuffer' })
